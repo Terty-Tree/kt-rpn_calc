19e069fd00dfc89d0c75c6166223e13adb59424b
Juan de Bruin <27200735+juan-db@users.noreply.github.com>
2018-09-27 12:40:54 +0200

    formatting

--- src/operator.kt
+++ src/operator.kt
@@ -1,6 +1,6 @@
 enum class Operator(private val identifier: String, private val operation: (Int, Int) -> Int) {
     PLUS("+", { a, b -> a + b }),
-    MINUS("-", { a, b -> a - b}),
+    MINUS("-", { a, b -> a - b }),
     MULTIPLY("*", { a, b -> a * b }),
     DIVIDE("/", { a, b -> if (b != 0) a / b else exit(5, "Error!") }),
     MOD("%", { a, b -> if (b != 0) a % b else exit(5, "Error!") });
